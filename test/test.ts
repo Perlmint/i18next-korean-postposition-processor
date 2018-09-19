b3dec97e2e1849ec1073174e8318db498aebf97b
MooYeol Presott Lee <mooyoul@gmail.com>
2018-09-19 21:13:02 +0900

    Add test case for custom modifier
    test case made by mooyoul

--- test/test.ts
+++ test/test.ts
@@ -38,6 +38,34 @@ describe("basic replacement test", () => {
         assert.equal(processor.process("1[[가]]"), "1가");
     });
 
+    it("custom modifier", () => {
+        const processor = new KoreanPostpositionProcessor();
+
+        // simplified dummy html tag remover
+        const htmlModifier = (str: string) => {
+            const stack: number[] = [];
+            let idx = str.length - 1;
+
+            for (; idx > 0; idx--) {
+                const ch = str[idx];
+                if (ch === ">") {
+                    stack.push(idx);
+                } else if (ch === "<") {
+                    stack.pop();
+                } else if (stack.length === 0) {
+                    return str.substring(0, idx + 1);
+                }
+            }
+
+            return str.substr(0, stack[stack.length - 1]);
+        };
+        assert.equal(processor.process("<b>레드벨벳</b>[[가]]"), "<b>레드벨벳</b>가");
+        processor.modifiers.push(htmlModifier);
+        assert.equal(processor.process("<b>레드벨벳</b>[[가]]"), "<b>레드벨벳</b>이");
+        processor.modifiers.pop();
+        assert.equal(processor.process("<b>레드벨벳</b>[[가]]"), "<b>레드벨벳</b>가");
+    });
+
     it("이/가", () => {
         const processor = new KoreanPostpositionProcessor();
 
